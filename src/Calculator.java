2f9976351f8a2fa27a9af8f1ce6db6040c7e7a3f
Trevor Smith <104841977+tjs-cap@users.noreply.github.com>
2023-04-20 16:01:39 +0100

    Includes the main class, an instance of the class being created inside of a main method and the test class.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2){
+        return num1 - num2;
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Program {
+public class Program {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(2,2));
+    }
 }
